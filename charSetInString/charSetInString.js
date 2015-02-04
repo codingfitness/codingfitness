46e5766687442a6c0e050a2527d087354b315c2d
John Tan <tantai.john@gmail.com>
2015-02-03 22:52:18 -0800

    Added charSetInString

---
+++ charSetInString/charSetInString.js
@@ -0,0 +1,13 @@
+// You are given a set of unique characters and a string.
+
+// Find the smallest substring of the string containing all the characters in the set.
+
+// ex:
+// Set : [a, b, c]
+// String : "abbcbcba"
+
+// Result: "cba"
+
+var charSetInString = function(chars, str){
+
+};
