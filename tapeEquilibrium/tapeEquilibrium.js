87f1fcee11ee7cd5beb7e10a1600f33268c2f4b5
Nikhil Ballaney <nballaney@gmail.com>
2015-02-04 23:02:33 -0800

    Remove solution

--- tapeEquilibrium/tapeEquilibrium.js
+++ tapeEquilibrium/tapeEquilibrium.js
@@ -34,17 +34,5 @@
 
 var findMinDifference = function(A) {
  // code here
- var i, ll = A.length, tot = 0, upto = 0, min = 9999;
 
-for (i=0; i<ll; i++) tot += A[i];
-
-for (i=0; i<ll-1; i++)
-{
-    upto += A[i];
-    var a1 = upto, a2 = tot - a1, dif = Math.abs(a1 - a2);
-    if (dif < min)
-         min = dif;
-}
-
-return min;
 };
