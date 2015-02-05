398df4edcb11921e73447f20d6d94a1aaa50c335
Nikhil Ballaney <nballaney@gmail.com>
2015-02-04 23:01:28 -0800

    Add tests

--- tapeEquilibrium/tapeEquilibrium.js
+++ tapeEquilibrium/tapeEquilibrium.js
@@ -32,6 +32,19 @@
 // Elements of input arrays can be modified.
 
 
-var solution = function(array) {
+var findMinDifference = function(A) {
  // code here
+ var i, ll = A.length, tot = 0, upto = 0, min = 9999;
+
+for (i=0; i<ll; i++) tot += A[i];
+
+for (i=0; i<ll-1; i++)
+{
+    upto += A[i];
+    var a1 = upto, a2 = tot - a1, dif = Math.abs(a1 - a2);
+    if (dif < min)
+         min = dif;
+}
+
+return min;
 };
